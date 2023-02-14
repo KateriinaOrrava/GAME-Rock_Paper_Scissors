83c4f2295e87ed9bc02c605b288caca2110ff079
Katerina Orrava <98848233+KateriinaOrrava@users.noreply.github.com>
2023-02-14 17:36:37 +0200

    stats styles

--- rock_paper_scissors_GAME/src/pages/GameStatistics/GameStatistics.tsx
+++ rock_paper_scissors_GAME/src/pages/GameStatistics/GameStatistics.tsx
@@ -1,5 +1,6 @@
 import { useTranslation, Trans } from 'react-i18next'; 
 import { useLocation } from "react-router-dom";
+import styles from './GameStatisctics.module.css'
 
 const GameStatistics= () => {
     const location = useLocation();
@@ -7,12 +8,12 @@ const GameStatistics= () => {
     const points = location.state.points
     const roundCounter = location.state.roundCounter
     console.log(userName.userName, points, roundCounter)
-
+console.log(userName.userName.userName)
     const { t, i18n } = useTranslation();
     return (
-        <div >
-<h1>hello</h1>
-
+        <div className={styles.stats}>
+            <h1>hello</h1>
+            {/* <p>PLAYER NAME: {userName.userName}</p> */}
         </div>
     )
 }
